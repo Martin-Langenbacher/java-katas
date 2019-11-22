b1d78b64188fcf43e3ecfc732af5c07b109fd33a
Martin Langenbacher <martin.langenbacher@googlemail.com>
2019-11-22 11:07:49 +0100

    Lösung erstellt

--- src/kata/CountLetters.java
+++ src/kata/CountLetters.java
@@ -1,4 +1,13 @@
 public class CountLetters {
 
+	public static int countLetters (String str, char s) {
+		int number = 0;
+		if (str == null) return -1;
+		for (int i = 0; i < str.length(); i++) {
+			if (str.charAt(i) == s) number = number +1; // number++
+		}
+		
+		return number;
+	}
     // TODO
 }
--- src/test/CountLettersTest.java
+++ src/test/CountLettersTest.java
@@ -3,10 +3,42 @@ import org.junit.Test;
 
 public class CountLettersTest {
 
-    @Test
-    public void testTrue() throws Exception {
-        Assert.assertEquals(true, true);
+    /* 
+     * "a" and 'a' returns 1
+     * "aaaaabbbaa" and 'a' returns 7
+     * "bbacbaaa" and 'c' returns 1
+     * "bbcc" and 'a' returns 0
+     * null and 'a' returns -1    * 
+     */
+	
+	@Test
+    public void testCase1() throws Exception {
+        Assert.assertEquals(1, CountLetters.countLetters("a", 'a'));
     }
 
-    // TODO
+	@Test
+    public void testCase2() throws Exception {
+        Assert.assertEquals(7, CountLetters.countLetters("aaaaabbbaa", 'a'));
+    }
+
+	@Test
+    public void testCase3() throws Exception {
+        Assert.assertEquals(1, CountLetters.countLetters("bbacbaaa", 'c'));
+    }
+
+	@Test
+    public void testCase4() throws Exception {
+        Assert.assertEquals(0, CountLetters.countLetters("bbcc", 'a'));
+    }
+	
+	@Test
+    public void testCase5() throws Exception {
+        Assert.assertEquals(-1, CountLetters.countLetters(null, 'a'));
+    }
+    
+	@Test
+    public void testCase6() throws Exception {
+        Assert.assertEquals(0, CountLetters.countLetters("", 'a'));
+    }
+
 }
